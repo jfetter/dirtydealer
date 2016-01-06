ce0006d96e36eb996c91671c602ab2fda0fff9ac
Drew <Drew@users-mbp-1055.attlocal.net>
2016-01-06 14:33:08 -0800

    working on login

--- models/User.js
+++ models/User.js
@@ -41,7 +41,7 @@ userSchema.statics.register = function(user, cb){
           cb(err, savedUser)
         })
       })
-    
+
     });
 });
 
@@ -57,8 +57,8 @@ userSchema.statics.login = function(user, cb){
 	User.find({$or: [{username: username}, {email: username}]}, function(err, userReturned){
 		console.log('userReturned', userReturned);
 		if(userReturned.length){
-			cb('no userfound')
-		}
+			cb('good!')
+		}else{console.log('bad')}
 		if(err){return console.log(err)}
 		bcrypt.compare(password, userReturned.password, function(err, res){
 				console.log('login', res)
