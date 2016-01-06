b9aae11667af7f5fbd0b4d76af0f70f43601af9d
Damon Kalaboukis <damonkala@yahoo.com>
2016-01-06 09:52:15 -0800

    Hashtagless

--- public/bundle.js
+++ public/bundle.js
@@ -3,10 +3,12 @@
 var app = angular.module('scaffoldApp', ['ui.router'])
 
 
-.config(function($stateProvider, $urlRouterProvider){
+.config(function($stateProvider, $urlRouterProvider, $locationProvider){
+	$locationProvider.html5Mode(true);
 	$urlRouterProvider.otherwise('/');
 	$stateProvider
 		.state('home', {url: '/', templateUrl: 'views/home/home.html', controller: 'homeCtrl'})
+		.state('login', {url: '/login', templateUrl: 'views/login/login.html', controller: 'loginCtrl'})
 })
 
 'use strict';
@@ -15,4 +17,12 @@ angular.module('scaffoldApp')
 .controller('homeCtrl', function($scope){
 	console.log('homeCtrl');
 	
+})
+
+'use strict';
+
+angular.module('scaffoldApp')
+.controller('loginCtrl', function($scope){
+	console.log('loginCtrl');
+
 })
--- source/module.js
+++ source/module.js
@@ -3,8 +3,10 @@
 var app = angular.module('scaffoldApp', ['ui.router'])
 
 
-.config(function($stateProvider, $urlRouterProvider){
+.config(function($stateProvider, $urlRouterProvider, $locationProvider){
+	$locationProvider.html5Mode(true);
 	$urlRouterProvider.otherwise('/');
 	$stateProvider
 		.state('home', {url: '/', templateUrl: 'views/home/home.html', controller: 'homeCtrl'})
+		.state('login', {url: '/login', templateUrl: 'views/login/login.html', controller: 'loginCtrl'})
 })
