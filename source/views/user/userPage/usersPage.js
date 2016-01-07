938985d4541c6fdc1b8a3b5d985ad62636bf9c62
Drew <Drew@users-mbp-1055.attlocal.net>
2016-01-07 15:08:56 -0800

    image upload working

--- source/views/user/userPage/usersPage.js
+++ source/views/user/userPage/usersPage.js
@@ -3,21 +3,63 @@
 angular.module('socialMockup')
 
 
-.controller('userPageCtrl', function($scope, $state, UserService, $cookies, $location){
+.controller('userPageCtrl', function($scope, $state, UserService, $cookies, jwtHelper, $location  ){
+	$scope.yourModel = {};
 	var cookies = $cookies.get('token');
+	var token = jwtHelper.decodeToken(cookies)
 	UserService.page($state.params.username)
 	.then(function(res) {
 		console.log("PARAMS", $state.params.name)
 		console.log(res.data.favorites)
 		$scope.user = res.data;
 		$scope.favorites = res.data.favorites;
+		$scope.isOwnPage = $scope.user.username ===token.username
+
 	}, function(err) {
 		console.error(err)
 	});
+
+
+
+
+	$scope.uploadFiles = function(file, errFiles) {
+        $scope.f = file;
+        $scope.errFile = errFiles && errFiles[0];
+        if (file) {
+            file.upload = Upload.upload({
+                url: `/imageUpload`,
+                data: {file: file}
+            });
+
+            file.upload.then(function (response) {
+                $timeout(function () {
+                    file.result = response.data;
+                });
+            }, function (response) {
+                if (response.status > 0)
+                    $scope.errorMsg = response.status + ': ' + response.data;
+            }, function (evt) {
+                file.progress = Math.min(100, parseInt(100.0 *
+                                         evt.loaded / evt.total));
+            });
+        }
+    }
+	// $scope.isOwnPage = false;
+	// $scope.imageAvailable = false;
+
+// 	= function(){
+// 		console.log('scope.user.username', $scope.user);
+// 		console.log('token user', token);
+// 		if($scope.user.username){return $scope.user.username ===token.username}
+// 		else{return false}
+// }
+	$scope.exposeData = function(){console.log($scope.myFile)}
 	UserService.isAuthed(cookies)
 	.then(function(res , err){
 		console.log(res.data)
 		 if (res.data === "authRequired"){$location.path('/login')}
 		 else{$scope.isLoggedIn = true;}
 	})
+
+
 });
