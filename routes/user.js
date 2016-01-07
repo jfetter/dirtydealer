ade18bb2875cf3aac8dcd3a5e2f08521ff010951
Damon Kalaboukis <damonkala@yahoo.com>
2016-01-06 19:11:44 -0800

    Users list

---
+++ routes/user.js
@@ -0,0 +1,20 @@
+'use strict'
+
+var express = require('express')
+var router = express.Router();
+
+var User = require('../models/User');
+
+router.get('/list', function(req, res){
+  User.find({}, function(err, users) {
+    res.status(err ? 400 : 200).send(err || users)
+  })
+})
+router.get('/page/:username', function(req, res){
+  User.find({'username' : req.params.username}, function(err, user) {
+    res.status(err ? 400 : 200).send(err || user)
+  })
+})
+
+
+module.exports = router;
