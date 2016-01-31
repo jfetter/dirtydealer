525a745670baabcadde210d43eb2da6e572f8592
Rachel Slater <rachelrobynslater@gmail.com>
2016-01-30 18:18:30 -0800

    styling for the userpage/profile - navigate to new gam or join existing

--- source/module.js
+++ source/module.js
@@ -3,8 +3,8 @@
 var app = angular.module('cardsAgainstHumanity', ['ui.router', 'angular-jwt', 'ngCookies','naif.base64', "base64", "firebase"])
 
 app.constant('ENV', {
-  // API_URL: 'http://localhost:3000'
-  API_URL: 'https://dry-dawn-94066.herokuapp.com'
+  API_URL: 'http://localhost:3000'
+  // API_URL: 'https://dry-dawn-94066.herokuapp.com'
 
 });
 
