b113b8ad51c49195340dc348a749ac90a3f55fe5
Jillian Fetter <jillian.fetter@gmail.com>
2016-02-01 10:00:08 -0800

    changed URL for site to new heroku

--- source/module.js
+++ source/module.js
@@ -4,7 +4,7 @@ var app = angular.module('cardsAgainstHumanity', ['ui.router', 'angular-jwt', 'n
 
 app.constant('ENV', {
    API_URL: 'http://localhost:3000'
-  //API_URL: 'https://dry-dawn-94066.herokuapp.com'
+  //API_URL: "https://secure-coast-55324.herokuapp.com"
 
 });
 
